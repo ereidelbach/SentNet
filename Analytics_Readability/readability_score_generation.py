ce21aaea65a61196840e5acc005b5cb0bf3d50e5
Eric Reidelbach <eric.reidelbach@gmail.com>
2018-05-28 20:31:17 -0400

    Created new Readability file with existing Python Packages

--- Analytics_Readability/readability_score_generation.py
+++ Analytics_Readability/readability_score_generation.py
@@ -5,16 +5,16 @@ Created on Mon May 28 14:42:29 2018
 
 @author: Eric Reidelbach
 
-description
-
-:REQUIRES:
-    
-:TODO:
+:DESCRIPTION:
     - The purpose of this script is to generate Interpretability and 
         Readability scores for text documents based on the following three indices:
             1. Flesch-Kincaid
             2. Gunning Fog
             3. Cloeman-Liau
+
+:REQUIRES:
+            
+:TODO:
 """
  
 #==============================================================================
@@ -22,19 +22,60 @@ description
 #==============================================================================
 import os   
 import pandas as pd
+import nltk
+nltk.download('punkt')
+nltk.download('cmudict')
+from textatistic import Textatistic
 from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.tokenize import RegexpTokenizer\
+from nltk.corpus import cmudict
+
+
 
 #==============================================================================
 # Function Definitions / Reference Variable Declaration
 #==============================================================================
-def FleschReadability(document):
-    # remove any words that are not from the English language
-    doc_cleaned = 
-    count_word = ""
-    count_sentance = ""
+def count_num_syllables(document):
+    syllable_count = 0
+    for word in document.split():
+        count = 0
+        vowels = 'aeiouy'
+        word = word.lower().strip(".:;?!")
+        if word[0] in vowels:
+            count +=1
+        for index in range(1,len(word)):
+            if word[index] in vowels and word[index-1] not in vowels:
+                count +=1
+        if word.endswith('e'):
+            count -= 1
+        if word.endswith('le'):
+            count+=1
+        if count == 0:
+            count +=1
+        syllable_count += count
+    return syllable_count
+
+'''
+    The Flesch Readability score is calculated as follows:
+    206.835 - [1.015 * (total words/total sentences)] - [84.6 * (total syllabes/total words)]
+'''
+def FleschReadability(document):  
+    # Determine the document's word count
+    count_word = len(document.split())
+#    tokenizer = RegexpTokenizer(r'\w+')
+#    count_word2 = len(tokenizer.tokenize(document))
     
+    # Determine the document's sentence count
+    count_sentence = len(nltk.sent_tokenize(document))
+    
+    # Determine the document's syllable count
+    count_syllables = count_num_syllables(document)
 
-
+    
+    # Calculate the Flesch Reading score
+    return (206.835 - (1.015 * (count_word/count_sentence)) - (84.6*(count_syllables/count_word)))
+    
+    
 def FleschKincaidGradeLevel(document):
     pass
 
@@ -48,7 +89,15 @@ os.chdir(r'E:\Projects\SentNet\Data')
 
 # Read in Training Data for scoring
 df = pd.read_csv('training_set_rel3.csv', encoding = "ISO-8859-1")
+document_list = list(df['essay'])
 
 # Compute Flesch readability ease
+FleschReadability(document_list[0])
+
+
+s = Textatistic(document_list[0])
+s.counts
+s.flesch_score
 
 # Compue Flesch-Kincaid grade level
+# [0.39 * (total words / total sentences)] + [11.8 + (total syllabes / total words)] - 15.59
---
+++ Analytics_Readability/readability_score_generation_packages.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python3.7
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 28 17:54:21 2018
+
+@author: Eric Reidelbach
+
+:DESCRIPTION:
+    - The purpose of this script is to generate Interpretability and 
+        Readability scores for text documents based on the following three indices:
+            1. Flesch-Kincaid
+            2. Gunning Fog
+            3. Cloeman-Liau  
+            
+:REQUIRES:
+    - Textatistic Python Package
+        https://pypi.org/project/textatistic/
+    - TextStat Python Package
+        https://pypi.org/project/textstat/
+:TODO:
+"""
+ 
+#==============================================================================
+# Package Import
+#==============================================================================
+import os   
+import pandas as pd
+from textstat.textstat import textstat
+from textatistic import Textatistic
+import textacy
+
+#==============================================================================
+# Function Definitions / Reference Variable Declaration
+#==============================================================================
+'''
+    Description:
+        Function that calculates scores for all relevant readability metrics
+        utilizing the TEXTATISTIC Python package.  
+    
+        Details on the package can be found here:
+            - https://pypi.org/project/textatistic/
+        
+    Input:
+        document (string): string containing the document to be scored
+        
+    Output:
+        document_dict (dictionary): dictionary containing the resulting scores
+            for the document along with counts for total number of words, 
+            syllables, and sentences
+'''
+def textatistic_scores(document):
+    try:
+        s = Textatistic(document)
+        document_dict = {}
+        document_dict['count_sent'] = s.sent_count
+        document_dict['count_sybl'] = s.sybl_count
+        document_dict['count_word'] = s.word_count
+        document_dict['score_dalechall'] = s.dalechall_score
+        document_dict['score_flesch'] = s.flesch_score
+        document_dict['score_fleschkincaid'] = s.fleschkincaid_score
+        document_dict['score_gunningfog'] = s.gunningfog_score
+        document_dict['score_smog'] = s.smog_score
+    except(ZeroDivisionError):
+        print('TEXTATISTIC -- Error with: ' + document)
+        document_dict['count_sent'] = 'N/A'
+        document_dict['count_sybl'] = 'N/A'
+        document_dict['count_word'] = 'N/A'
+        document_dict['score_dalechall'] = 'N/A'
+        document_dict['score_flesch'] = 'N/A'
+        document_dict['score_fleschkincaid'] = 'N/A'
+        document_dict['score_gunningfog'] = 'N/A'
+        document_dict['score_smog'] = 'N/A'      
+    return document_dict
+
+'''
+    Description:
+        Function that calculates scores for all relevant readability metrics
+        utilizing the TEXTSTAT Python package.  
+    
+        Details on the package can be found here:
+            - https://pypi.org/project/textstat/
+        
+    Input:
+        document (string): string containing the document to be scored
+        
+    Output:
+        document_dict (dictionary): dictionary containing the resulting scores
+            for the document along with counts for total number of words, 
+            syllables, and sentences
+'''    
+def textstat_scores(document):
+    document_dict = {}
+    document_dict['count_sent'] = textstat.sentence_count(document)
+    document_dict['count_sybl'] = textstat.syllable_count(document)
+    document_dict['count_word'] = textstat.lexicon_count(document)
+    try:
+        document_dict['score_dalechall'] = textstat.dale_chall_readability_score(document)
+        document_dict['score_flesch'] = textstat.flesch_reading_ease(document)
+        document_dict['score_fleschkincaid'] = textstat.flesch_kincaid_grade(document)
+        document_dict['score_gunningfog'] = textstat.gunning_fog(document)
+        document_dict['score_smog'] = textstat.smog_index(document)
+    except(ZeroDivisionError):
+        print('TEXTSTAT -- Error with: ' + document)
+        document_dict['score_dalechall'] = 'N/A'
+        document_dict['score_flesch'] = 'N/A'
+        document_dict['score_fleschkincaid'] = 'N/A'
+        document_dict['score_gunningfog'] = 'N/A'
+        document_dict['score_smog'] = 'N/A'    
+    return document_dict
+
+'''
+    Description:
+        Function that calculates scores for all relevant readability metrics
+        utilizing the TEXTACY Python package.  
+    
+        Details on the package can be found here:
+            - https://chartbeat-labs.github.io/textacy/index.html
+        
+    Input:
+        document (string): string containing the document to be scored
+        
+    Output:
+        document_dict (dictionary): dictionary containing the resulting scores
+            for the document along with counts for total number of words, 
+            syllables, and sentences
+'''
+def textacy_scores(document):
+    document_dict = {}
+    document_dict['count_sent'] = textacy.n_sents(document)
+    document_dict['count_sybl'] = textacy.n_syllables(document)
+    document_dict['count_word'] = textacy.n_words(document)
+    try:
+        document_dict['score_colemanliau'] = textacy.coleman_liau_index(document)
+        document_dict['score_dalechall'] = 'N/A'
+        document_dict['score_flesch'] = textacy.flesch_reading_ease(document)
+        document_dict['score_fleschkincaid'] = textacy.flesch_kincaid_grade_level(document)
+        document_dict['score_gunningfog'] = textacy.gunning_fog_index(document)
+        document_dict['score_smog'] = textacy.smog_index(document)
+    except(ZeroDivisionError):
+        print('TEXTACY -- Error with: ' + document)
+        document_dict['score_colemanliau'] = 'N/A'
+        document_dict['score_dalechall'] = 'N/A'
+        document_dict['score_flesch'] = 'N/A'
+        document_dict['score_fleschkincaid'] = 'N/A'
+        document_dict['score_gunningfog'] = 'N/A'
+        document_dict['score_smog'] = 'N/A'    
+    return document_dict
+    
+#==============================================================================
+# Working Code
+#==============================================================================
+
+# Set the project working directory
+os.chdir(r'E:\Projects\SentNet\Data')
+
+# Read in Training Data for scoring
+df = pd.read_csv('training_set_rel3.csv', encoding = "ISO-8859-1")
+document_list = list(df['essay'])
+
+# Create a list for storing the scores of every document in this training set
+score_list = []
+
+# Compute scores with both Textatistic and Textstat
+for document in document_list[0:5]:
+    document_dict = {}
+    document_dict['text'] = document
+    
+    # retrieve the scores from all popular readability packages:
+    #   textatistic, textstat, and textacy
+    document_dict['textatistic'] = textatistic_scores(document)
+    document_dict['textstat'] = textstat_scores(document)
+    document_dict['textacy'] = textacy_scores(document)
+    
+    # store the results
+    score_list.append(document_dict)
+    
+    # count our progress in the list
+    if document_list.index(document) % 100 == 0:
+        print(document_list.index(document))
+    
