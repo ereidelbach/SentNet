a29535c37d36af61e3376b85bf922f107d4a726d
Eric Reidelbach <eric.reidelbach@gmail.com>
2018-06-20 22:51:57 -0400

    Formatting changes for library import of several functions

--- Data_Modeling/SentNet_Training_Master.py
+++ Data_Modeling/SentNet_Training_Master.py
@@ -6,6 +6,7 @@ Created on Tue Jun 12 22:56:51 2018
 """
 
 # Read in the required packages
+import math
 import numpy as np
 from pathlib import Path
 import pandas as pd
@@ -13,21 +14,29 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 
 # import functions from other python files
+
 from Data_Ingest.SentNet_Data_Prep_Functions import Ingest_Training_Data
+
 #from SentNet_Data_Feature_Extraction_V3 import Readability_Features
+
 from Data_Preprocessing.SentNet_Data_Feature_Extraction import Word_Features, \
 Word_Edge_Features, Word_Centrality_Features, Synset_Features, \
-Synset_Edge_Features, Synset_Centrality_Features
-from Data_Modeling.Term_Clustering import Clustering_Features, Cluster_Concentrations, \
-Synset_Clustering_Features, Synset_Concentrations
-from Data_Modeling.SentNet_Document_Matching import Document_Matching_Training
-from Data_Modeling.SentNet_Document_Similarity import Document_Similarity_Training
+Synset_Edge_Features, Synset_Centrality_Features, Readability_Features
 
-###############################################################################################
+from Data_Modeling.Term_Clustering import Clustering_Features, \
+Cluster_Concentrations, Synset_Clustering_Features, Synset_Concentrations
+
+from Data_Modeling.SentNet_Document_Matching import \
+Document_Matching_Training, Document_Matching_Testing
+
+from Data_Modeling.SentNet_Document_Similarity import \
+Document_Similarity_Training, Document_Similarity_Testing
+
+###############################################################################
 # Data Ingest 
-###############################################################################################
+###############################################################################
 
-################################## .Docx Data Ingest ##########################################
+################################## .Docx Data Ingest ##########################
 
 # This assumes the Set1 Data is in the path: SentNet\Data\Set1\docx\Images
 path_data = Path('Data','Set1','docx','Images')
@@ -38,13 +47,15 @@ data_raw = Ingest_Training_Data(path_data, path_images)
 
 temp_list = []
 for index, row in data_raw.iterrows():
-    # luckily, everything is broken up by line splits so let's break up the text that way
+    # luckily, everything is broken up by line splits so let's break up the 
+    #   text that way
     doc_elements = row['Doc_Text'].split('\n')
     # delete empty elements
     doc_elements = [x for x in doc_elements if x != '']
     # remove any rows that are similar to 'Image 1', 'Image 2', etc...
     doc_elements = [x for x in doc_elements if 'Image ' not in x]
-    # create a dictionary where the key is the variable name and the value is score/text from the document
+    # create a dictionary where the key is the variable name and the value is 
+    #   score/text from the document
     doc_dict = {}
     for (key, value) in zip(doc_elements[::2], doc_elements[1::2]):
         doc_dict[key] = value
@@ -58,7 +69,7 @@ for index, row in data_raw.iterrows():
 data = pd.DataFrame(temp_list)
 
 '''
-############################### Spreadsheet Data Ingest #######################################
+############################### Spreadsheet Data Ingest #######################
 # Alternatively read in data from a pre-populated spreadsheet (or database table)
 # This assumes we wish to read the csv located in the path: 
 #      SentNet\Data\Set1
@@ -70,19 +81,20 @@ data = data[data['essay_set']==1]
 print("Done Data Ingest")
 '''
 
-###############################################################################################
+###############################################################################
 # Train and Test Split
-###############################################################################################
+###############################################################################
 
 train, test = train_test_split(data, test_size=0.2)
 train.reset_index(drop=True, inplace=True)
 test.reset_index(drop=True, inplace=True)
 
-###############################################################################################
+###############################################################################
 # Feature Extraction 
-###############################################################################################
+###############################################################################
 
-# Define the minimum threshold (the number of documents a feature must appear in) for a feature to be included in our analysis
+# Define the minimum threshold (the number of documents a feature must appear 
+#   in) for a feature to be included in our analysis
 limit = round(0.02*len(train))
 target = 'domain1_score'
 doc = 'essay'
@@ -92,8 +104,10 @@ readability_features = Readability_Features(train, doc)
 
 # Matching (most similar) document score (Doc2Vec)
 matching_docs = Document_Matching_Training(train, doc, target, limit=0)
-gensim_model_matching = matching_docs['gensim_model'] # Saving this model to apply to testing/scoring data later on
-scores_join = matching_docs['scores_join'] # Saving this as an input to testing/scoring data later on 
+# Saving this model to apply to testing/scoring data later on
+gensim_model_matching = matching_docs['gensim_model'] 
+# Saving this as an input to testing/scoring data later on 
+scores_join = matching_docs['scores_join'] 
 matching_docs = matching_docs['train_estimates']
 
 # General Similarity score (Doc2Vec)
@@ -123,13 +137,14 @@ synset_features = synset_features['synset_matrix_features']
 synset_edge_features = Synset_Edge_Features(train, doc, limit)
 synset_edge_list = synset_edge_features['master_edge_list_synset']
 synset_edge_features = synset_edge_features['edges_matrix_features_synset']
-synset_centrality_features = Synset_Centrality_Features(train, doc, selected_synsets)
+synset_centrality_features = Synset_Centrality_Features(
+        train, doc, selected_synsets)
 synset_clusters = Synset_Clustering_Features(synset_edge_list, limit)
 synset_cluster_features = Synset_Concentrations(train, doc, synset_clusters)
 
-###############################################################################################
+###############################################################################
 # Modeling 
-###############################################################################################
+###############################################################################
 
 # Merge feature space to create training dataset
 train_data = pd.concat([readability_features, 
@@ -141,7 +156,7 @@ train_data = pd.concat([readability_features,
                         word_cluster_features,
                         synset_features,
                         synset_edge_features,
-                        synset_centrality_matrix,
+                        synset_centrality_features,
                         synset_cluster_features
                         ]
                         ,axis=1)
@@ -155,15 +170,20 @@ rfc = RandomForestClassifier(n_estimators=10000, n_jobs=7)
 rfc.fit(train_data,train[target])
 
 # estimate the feature importance
-feature_importance = pd.DataFrame(rfc.feature_importances_,columns=['importance'])
-feature_importance = pd.concat([pd.DataFrame(list(train_data.columns)),feature_importance],axis=1)
-modeling_features = list(feature_importance[feature_importance['importance']>=0.0001][0])
+feature_importance = pd.DataFrame(
+        rfc.feature_importances_,columns=['importance'])
+feature_importance = pd.concat([pd.DataFrame(
+        list(train_data.columns)),feature_importance],axis=1)
+modeling_features = list(
+        feature_importance[feature_importance['importance']>=0.0001][0])
 
 # train scoring model
-rfc = RandomForestClassifier(n_estimators=2000, n_jobs=7, max_depth=8, min_samples_leaf=3)
+rfc = RandomForestClassifier(
+        n_estimators=2000, n_jobs=7, max_depth=8, min_samples_leaf=3)
 rfc.fit(train_data[modeling_features],train[target])
 feature_importance2 = pd.DataFrame(rfc.feature_importances_,columns=['importance'])
-feature_importance2 = pd.concat([pd.DataFrame(list(train_data[modeling_features].columns)),feature_importance2],axis=1)
+feature_importance2 = pd.concat([pd.DataFrame(list(
+        train_data[modeling_features].columns)),feature_importance2],axis=1)
 
 # calculate predicted values for training set
 preds = rfc.predict(train_data[modeling_features])
@@ -172,37 +192,51 @@ print("TRAINING")
 print(pred_crosstab)
 print(" ")
 
-################################################################################################
+###############################################################################
 # Scoring 
-###############################################################################################
+###############################################################################
 
 # Calculate readability features for each document
 readability_features_score = Readability_Features(test, doc)
 
 # Matching (most similar) document score (Doc2Vec)
-matching_docs_score = Document_Matching_Testing(test, doc, target, scores_join, gensim_model_matching, limit=0)
+matching_docs_score = Document_Matching_Testing(
+        test, doc, target, scores_join, gensim_model_matching, limit=0)
 
 # General Similarity score (Doc2Vec)
-similar_docs_score = Document_Similarity_Testing(train, doc, target, gensim_model_similarity, limit=0)
+similar_docs_score = Document_Similarity_Testing(
+        train, doc, target, gensim_model_similarity, limit=0)
 
 # Calculate word level features for each document
-word_features_score = Word_Features(test, doc, limit=0)['word_matrix_features']
-word_features_score = word_features_score[word_features_score.columns.intersection(selected_words)]
+word_features_score = Word_Features(
+        test, doc, limit=0)['word_matrix_features']
+word_features_score = word_features_score[
+        word_features_score.columns.intersection(selected_words)]
 
 # Calculate word graph features for each document
-word_edge_features_score = Word_Edge_Features(test, doc, limit)['edges_matrix_features']
-word_edge_features_score = word_edge_features_score[word_edge_features_score.columns.intersection(list(set(word_edge_list['edge_id'])))]
-word_centrality_features_score = Word_Centrality_Features(test, doc, selected_words)
+word_edge_features_score = Word_Edge_Features(
+        test, doc, limit)['edges_matrix_features']
+word_edge_features_score = word_edge_features_score[
+        word_edge_features_score.columns.intersection(
+                list(set(word_edge_list['edge_id'])))]
+word_centrality_features_score = Word_Centrality_Features(
+        test, doc, selected_words)
 word_cluster_features_score = Cluster_Concentrations(test, doc, word_clusters)
 
 # Calculate synset level features for each document
-synset_features_score = Synset_Features(test, doc, limit=0)['synset_matrix_features']
-synset_features_score = synset_features_score[synset_features_score.columns.intersection(selected_synsets)]
+synset_features_score = Synset_Features(
+        test, doc, limit=0)['synset_matrix_features']
+synset_features_score = synset_features_score[
+        synset_features_score.columns.intersection(selected_synsets)]
 
 # Calculate synset graph features for each document
-synset_edge_features_score = Synset_Edge_Features(test, doc, limit)['edges_matrix_features_synset']
-synset_edge_features_score = synset_edge_features_score[synset_edge_features_score.columns.intersection(list(set(synset_edge_list['edge_id'])))]
-synset_centrality_features_score = Synset_Centrality_Features(test, doc, selected_synsets)
+synset_edge_features_score = Synset_Edge_Features(
+        test, doc, limit)['edges_matrix_features_synset']
+synset_edge_features_score = synset_edge_features_score[
+        synset_edge_features_score.columns.intersection(
+                list(set(synset_edge_list['edge_id'])))]
+synset_centrality_features_score = Synset_Centrality_Features(
+        test, doc, selected_synsets)
 synset_cluster_features_score = Synset_Concentrations(test, doc, synset_clusters)
 
 # Preparing dataset for predictions
@@ -248,7 +282,8 @@ accuracy_rate = accurate_count/total_count
 print("Total Accuracy Rate: "+str(accuracy_rate))
 
 accuracy = pd.concat([pd.DataFrame(test[target]), pd.DataFrame(preds)],axis=1)
-accuracy['correct_pred'] = accuracy.apply(lambda row: math.sqrt((row[target]-row[0])**2), axis=1)
+accuracy['correct_pred'] = accuracy.apply(
+        lambda row: math.sqrt((row[target]-row[0])**2), axis=1)
 accuracy['correct_buffer'] = accuracy['correct_pred']<=1
 model_error = accuracy['correct_pred'].mean()
 print("Model Error: "+str(model_error))
