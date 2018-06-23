2b7b8009486e2bde8d9dcca254c532b2df244ca7
Taylor Corbett <taylor.corbett@visionistinc.com>
2018-06-23 03:11:46 +0000

    Changing variable names so they can be differentiated within a dataframe

--- Analytics_Readability/readability_score_generation_packages.py
+++ Analytics_Readability/readability_score_generation_packages.py
@@ -62,24 +62,24 @@ def textatistic_scores(document):
     document_dict = {}
     try:
         s = Textatistic(document)
-        document_dict['count_sent'] = s.sent_count
-        document_dict['count_sybl'] = s.sybl_count
-        document_dict['count_word'] = s.word_count
-        document_dict['score_dalechall'] = s.dalechall_score
-        document_dict['score_flesch'] = s.flesch_score
-        document_dict['score_fleschkincaid'] = s.fleschkincaid_score
-        document_dict['score_gunningfog'] = s.gunningfog_score
-        document_dict['score_smog'] = s.smog_score
+        document_dict['tt_count_sent'] = s.sent_count
+        document_dict['tt_count_sybl'] = s.sybl_count
+        document_dict['tt_count_word'] = s.word_count
+        document_dict['tt_score_dalechall'] = s.dalechall_score
+        document_dict['tt_score_flesch'] = s.flesch_score
+        document_dict['tt_score_fleschkincaid'] = s.fleschkincaid_score
+        document_dict['tt_score_gunningfog'] = s.gunningfog_score
+        document_dict['tt_score_smog'] = s.smog_score
     except(ZeroDivisionError):
         print('TEXTATISTIC -- Error with: ' + document)
-        document_dict['count_sent'] = 'N/A'
-        document_dict['count_sybl'] = 'N/A'
-        document_dict['count_word'] = 'N/A'
-        document_dict['score_dalechall'] = 'N/A'
-        document_dict['score_flesch'] = 'N/A'
-        document_dict['score_fleschkincaid'] = 'N/A'
-        document_dict['score_gunningfog'] = 'N/A'
-        document_dict['score_smog'] = 'N/A'      
+        document_dict['tt_count_sent'] = 'N/A'
+        document_dict['tt_count_sybl'] = 'N/A'
+        document_dict['tt_count_word'] = 'N/A'
+        document_dict['tt_score_dalechall'] = 'N/A'
+        document_dict['tt_score_flesch'] = 'N/A'
+        document_dict['tt_score_fleschkincaid'] = 'N/A'
+        document_dict['tt_score_gunningfog'] = 'N/A'
+        document_dict['tt_score_smog'] = 'N/A'      
     return document_dict
 
 def textstat_scores(document):
@@ -100,22 +100,22 @@ def textstat_scores(document):
             syllables, and sentences
     '''      
     document_dict = {}
-    document_dict['count_sent'] = textstat.sentence_count(document)
-    document_dict['count_sybl'] = textstat.syllable_count(document)
-    document_dict['count_word'] = textstat.lexicon_count(document)
+    document_dict['ts_count_sent'] = textstat.sentence_count(document)
+    document_dict['ts_count_sybl'] = textstat.syllable_count(document)
+    document_dict['ts_count_word'] = textstat.lexicon_count(document)
     try:
-        document_dict['score_dalechall'] = textstat.dale_chall_readability_score(document)
-        document_dict['score_flesch'] = textstat.flesch_reading_ease(document)
-        document_dict['score_fleschkincaid'] = textstat.flesch_kincaid_grade(document)
-        document_dict['score_gunningfog'] = textstat.gunning_fog(document)
-        document_dict['score_smog'] = textstat.smog_index(document)
+        document_dict['ts_score_dalechall'] = textstat.dale_chall_readability_score(document)
+        document_dict['ts_score_flesch'] = textstat.flesch_reading_ease(document)
+        document_dict['ts_score_fleschkincaid'] = textstat.flesch_kincaid_grade(document)
+        document_dict['ts_score_gunningfog'] = textstat.gunning_fog(document)
+        document_dict['ts_score_smog'] = textstat.smog_index(document)
     except(ZeroDivisionError):
         print('TEXTSTAT -- Error with: ' + document)
-        document_dict['score_dalechall'] = 'N/A'
-        document_dict['score_flesch'] = 'N/A'
-        document_dict['score_fleschkincaid'] = 'N/A'
-        document_dict['score_gunningfog'] = 'N/A'
-        document_dict['score_smog'] = 'N/A'    
+        document_dict['ts_score_dalechall'] = 'N/A'
+        document_dict['ts_score_flesch'] = 'N/A'
+        document_dict['ts_score_fleschkincaid'] = 'N/A'
+        document_dict['ts_score_gunningfog'] = 'N/A'
+        document_dict['ts_score_smog'] = 'N/A'    
     return document_dict
         
 #def textacy_scores(document):
@@ -147,22 +147,22 @@ def textstat_scores(document):
 #    doc = textacy.Doc(document)
 #    ts = textacy.TextStats(doc)
 #    document_dict = {}
-#    document_dict['count_sent'] = ts.n_sents
-#    document_dict['count_sybl'] = ts.n_syllables
-#    document_dict['count_word'] = ts.n_words
+#    document_dict['ty_count_sent'] = ts.n_sents
+#    document_dict['ty_count_sybl'] = ts.n_syllables
+#    document_dict['ty_count_word'] = ts.n_words
 #    try:
-#        document_dict['score_colemanliau'] = ts.coleman_liau_index
-#        document_dict['score_dalechall'] = 'N/A'
-#        document_dict['score_flesch'] = ts.flesch_reading_ease
-#        document_dict['score_fleschkincaid'] = ts.flesch_kincaid_grade_level
-#        document_dict['score_gunningfog'] = ts.gunning_fog_index
-#        document_dict['score_smog'] = ts.smog_index
+#        document_dict['ty_score_colemanliau'] = ts.coleman_liau_index
+#        document_dict['ty_score_dalechall'] = 'N/A'
+#        document_dict['ty_score_flesch'] = ts.flesch_reading_ease
+#        document_dict['ty_score_fleschkincaid'] = ts.flesch_kincaid_grade_level
+#        document_dict['ty_score_gunningfog'] = ts.gunning_fog_index
+#        document_dict['ty_score_smog'] = ts.smog_index
 #    except(ZeroDivisionError):
 #        print('TEXTACY -- Error with: ' + document)
-#        document_dict['score_colemanliau'] = 'N/A'
-#        document_dict['score_dalechall'] = 'N/A'
-#        document_dict['score_flesch'] = 'N/A'
-#        document_dict['score_fleschkincaid'] = 'N/A'
-#        document_dict['score_gunningfog'] = 'N/A'
-#        document_dict['score_smog'] = 'N/A'    
+#        document_dict['ty_score_colemanliau'] = 'N/A'
+#        document_dict['ty_score_dalechall'] = 'N/A'
+#        document_dict['ty_score_flesch'] = 'N/A'
+#        document_dict['ty_score_fleschkincaid'] = 'N/A'
+#        document_dict['ty_score_gunningfog'] = 'N/A'
+#        document_dict['ty_score_smog'] = 'N/A'    
 #    return document_dict
